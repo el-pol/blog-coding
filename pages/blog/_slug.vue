f750fc374d6b2f627063237819f195f2af5a5935
Pol Milian <pol@vidjet.io>
2021-01-30 12:43:25 +0100

    added metadata

--- pages/blog/_slug.vue
+++ pages/blog/_slug.vue
@@ -93,6 +93,36 @@ export default {
       next
     }
   },
+  head() {
+    return {
+      title: this.article.title,
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: this.article.description
+        },
+        // Open Graph
+        { hid: 'og:title', property: 'og:title', content: this.article.title },
+        {
+          hid: 'og:description',
+          property: 'og:description',
+          content: this.article.description
+        },
+        // Twitter Card
+        {
+          hid: 'twitter:title',
+          name: 'twitter:title',
+          content: this.article.title
+        },
+        {
+          hid: 'twitter:description',
+          name: 'twitter:description',
+          content: this.article.description
+        }
+      ]
+    }
+  },
   methods: {
     formatDate(date) {
       const options = { year: 'numeric', month: 'long', day: 'numeric' }
