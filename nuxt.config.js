fcdbdcf8cd472ccffa909eadd03171314597b047
Pol Milian <pol@vidjet.io>
2021-04-02 13:42:54 +0200

    added OG properties

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,24 +3,49 @@ export default {
    ** Nuxt target
    ** See https://nuxtjs.org/api/configuration-target
    */
-  target: 'static',
+  target: "static",
   /*
    ** Headers of the page
    ** See https://nuxtjs.org/api/configuration-head
    */
   head: {
-    title: 'Bulenya',
+    title: "Bulenya",
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
       {
-        hid: 'description',
-        name: 'description',
+        hid: "description",
+        name: "description",
         content:
-          'Bulenya (The Stranger) is a blog about traveling, food and existence.'
-      }
+          "Bulenya (The Stranger) is a blog about traveling, food and existence.",
+      },
+      { property: "og:site_name", content: "Bulenya" },
+      { hid: "og:type", property: "og:type", content: "website" },
+      {
+        hid: "og:url",
+        property: "og:url",
+        content: "https://bulenya.xyz",
+      },
+      {
+        hid: "og:title",
+        property: "og:title",
+        content: "Bulenya",
+      },
+      {
+        hid: "og:description",
+        property: "og:description",
+        content:
+          "Bulenya (The Stranger) is a blog about traveling, food and existence.",
+      },
+      {
+        hid: "og:image",
+        property: "og:image",
+        content: "/mtgillenpol.jpeg",
+      },
+      { property: "og:image:width", content: "740" },
+      { property: "og:image:height", content: "300" },
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
   },
   /*
    ** Global CSS
@@ -41,20 +66,20 @@ export default {
    */
   buildModules: [
     // Doc: https://github.com/nuxt-community/eslint-module
-    '@nuxtjs/eslint-module',
+    "@nuxtjs/eslint-module",
     // Doc: https://github.com/nuxt-community/stylelint-module
-    '@nuxtjs/stylelint-module',
+    "@nuxtjs/stylelint-module",
     // Doc: https://github.com/nuxt-community/nuxt-tailwindcss
-    '@nuxtjs/tailwindcss',
-    '@nuxt/image',
-    '@nuxtjs/google-analytics'
+    "@nuxtjs/tailwindcss",
+    "@nuxt/image",
+    "@nuxtjs/google-analytics",
   ],
   /*
    ** Nuxt.js modules
    */
   modules: [
     // Doc: https://github.com/nuxt/content
-    '@nuxt/content'
+    "@nuxt/content",
   ],
   /*
    ** Content module configuration
@@ -67,6 +92,6 @@ export default {
    */
   build: {},
   googleAnalytics: {
-    id: 'UA-113570864-2'
-  }
-}
+    id: "UA-113570864-2",
+  },
+};
